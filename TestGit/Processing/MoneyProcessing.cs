4bbac3378ddb3c7d1527a6fb4ffb188533835788
Samir Guendoul <Samir.Guendoul@tns-ilres.com>
2021-09-15 17:54:38 +0200

    add repertoire

---
+++ TestGit/Processing/MoneyProcessing.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestGit.Model;
+
+namespace TestGit.Processing
+{
+    public class MoneyProcessing
+    {
+        public MoneyModel GetMoney(string id)
+        {
+            MoneyModel item = null;
+
+            return item;
+        }
+    }
+}
